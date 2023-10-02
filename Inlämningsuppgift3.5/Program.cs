ebd1202cae6205e7b45426c1ecbe44afa955724b
molo06002 <molo06002@E-PW0234KQ>
2023-10-02 14:03:48 +0200

    Add project files.

---
+++ Inlämningsuppgift3.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace inlämningsuppgift3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in  tal nmr 1: ");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.WriteLine("Skriv in tal nmr 2: ");
+            string r = Console.ReadLine();
+            int R = Convert.ToInt32(r);
+            int addition = y + R;
+            Console.Clear();
+            Console.WriteLine("Addition: " + addition);
+            int subtraktion = y - R;
+            Console.WriteLine("Subtraktion: " + subtraktion);
+            int multiplikation = y * R;
+            Console.WriteLine("Multiplikation: " + multiplikation);
+            int division = y / R;
+            Console.WriteLine("Division: " + division);
+        }
+    }
+}
